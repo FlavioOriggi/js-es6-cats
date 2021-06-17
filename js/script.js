6f412afedd7c26095c81d080d0ef01922d6dcc45
Flavio <flavio.origgi@outlook.it>
2021-06-17 17:44:08 +0200

    milestone 1

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Milestone 1: Definire un array di oggetti; ogni oggetto rappresenta un gatto, che è caratterizzato 
+// da: nome, età, colore e sesso.
+
+const gatti = [
+    {name: 'Romeo', age:1.6, color:'#3e4649', gender:'male'},
+    {name: 'Micia', age:0.8, color:'#ebdece', gender:'female'},
+    {name: 'Garfield', age:1.5, color:'#b46329', gender:'male'},
+    {name: 'Micietta', age:2.5, color:'#d1d0ce', gender:'female'},
+    {name: 'Mario', age:3, color:'#5c5632', gender:'male'},
+    {name: 'Gattina', age:4, color:'#0d0805', gender:'female'}
+];
+
